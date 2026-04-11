121ef3c339aa6a4664b910ca37959baf47f3d318
saurav7747 <izsaurav777@gmail.com>
2026-04-11 11:42:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,1 +1,8 @@
+import { auth } from "./firebase.js";
+import { signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+window.logout = function(){
+  signOut(auth).then(()=>{
+    location.href="../index.html";
+  });
+};
