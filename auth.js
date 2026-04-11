7be87f4d70f12755cd53491c9fb0d0de769c8045
saurav7747 <izsaurav777@gmail.com>
2026-04-11 11:38:10 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,36 @@
+import { auth, db } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+import {
+  setDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+onAuthStateChanged(auth, (user)=>{
+  if(user){
+    window.location.href = "main.html";
+  }
+});
+
+window.signupUser = async function(){
+  let u = username.value;
+  let p = password.value;
+
+  let res = await createUserWithEmailAndPassword(auth, u+"@app.com", p);
+
+  await setDoc(doc(db,"users",res.user.uid),{
+    uid:res.user.uid,
+    username:u
+  });
+};
+
+window.loginUser = function(){
+  let u = username.value;
+  let p = password.value;
+
+  signInWithEmailAndPassword(auth, u+"@app.com", p);
+};
