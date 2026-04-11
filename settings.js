093199da8edcf8caffd006d9e2414896c5d4be91
saurav7747 <izsaurav777@gmail.com>
2026-04-11 13:10:27 +0530

    Delete settings.js

--- settings.js
+++
@@ -1,8 +0,0 @@
-import { auth } from "./firebase.js";
-import { signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-window.logout = function(){
-  signOut(auth).then(()=>{
-    location.href="../index.html";
-  });
-};
