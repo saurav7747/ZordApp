66e08b2e18c637429112c50c2aab6386b3942a18
saurav7747 <izsaurav777@gmail.com>
2026-04-11 13:59:39 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyABxUdNL1iHxgRGXNy9xbh6c9Db0nyS-J0",
   authDomain: "zordapp-467e9.firebaseapp.com",
@@ -13,7 +7,7 @@ const firebaseConfig = {
   appId: "1:774552789251:web:c847cb42fb2e4ce7bc4936"
 };
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const auth = firebase.auth();
+const db = firebase.firestore();
