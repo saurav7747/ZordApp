d66837f3a55f50d07a576ccae3f49b207eb72dc4
saurav7747 <izsaurav777@gmail.com>
2026-04-11 14:13:52 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,12 @@
-// Signup
+// 🔥 Auto login check (already logged in ho to direct main page)
+auth.onAuthStateChanged(function(user){
+  if(user){
+    window.location.href = "main.html";
+  }
+});
+
+
+// 🔥 SIGNUP FUNCTION
 function signup(){
   let u = document.getElementById("username").value;
   let p = document.getElementById("password").value;
@@ -11,21 +19,26 @@ function signup(){
   let email = u + "@app.com";
 
   auth.createUserWithEmailAndPassword(email, p)
-  .then((res)=>{
+  .then(function(res){
+
+    // 🔥 Firestore me user save
     db.collection("users").doc(res.user.uid).set({
       uid: res.user.uid,
       username: u
     });
 
     alert("Signup success");
+
+    // 👉 direct main page
+    window.location.href = "main.html";
   })
-  .catch(err=>{
-    alert(err.message);
+  .catch(function(error){
+    alert(error.message);
   });
 }
 
 
-// Login
+// 🔥 LOGIN FUNCTION
 function login(){
   let u = document.getElementById("username").value;
   let p = document.getElementById("password").value;
@@ -38,10 +51,14 @@ function login(){
   let email = u + "@app.com";
 
   auth.signInWithEmailAndPassword(email, p)
-  .then(()=>{
+  .then(function(){
+
     alert("Login success");
+
+    // 👉 main page redirect
+    window.location.href = "main.html";
   })
-  .catch(err=>{
-    alert(err.message);
+  .catch(function(error){
+    alert(error.message);
   });
 }
