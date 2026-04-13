5cfce48c9f7c5da2a349c937d4bc61bdf04a45ba
saurav7747 <izsaurav777@gmail.com>
2026-04-13 10:20:19 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,16 @@
-// Firebase Config
+// Firebase CDN (index.html me add hona chahiye)
+// <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+// <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+// <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
+
+// Your config
 const firebaseConfig = {
-  apiKey: "AIzaSyABxUdNL1iHxgRGXNy9xbh6c9Db0nyS-J0",
-  authDomain: "zordapp-467e9.firebaseapp.com",
-  projectId: "zordapp-467e9",
-  storageBucket: "zordapp-467e9.firebasestorage.app",
-  messagingSenderId: "774552789251",
-  appId: "1:774552789251:web:c847cb42fb2e4ce7bc4936"
+  apiKey: "AIzaSyDTNwG8GCZqOXKasZ5XfS5eqT2hDrzxD0Q",
+  authDomain: "zordapp2.firebaseapp.com",
+  projectId: "zordapp2",
+  storageBucket: "zordapp2.firebasestorage.app",
+  messagingSenderId: "279072692523",
+  appId: "1:279072692523:web:eb6fdd5d9ad96a9e08cd6e"
 };
 
 // Initialize Firebase
